78db106aec422f0e7dbcc1f75bea0cccdaf00615
Joshua Soberg <j15soberg@gmail.com>
2021-05-12 11:35:33 -0400

    Add initial project files

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.10"
+    application
+}
+
+group = "com.soberg.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClassName = "MainKt"
+}
